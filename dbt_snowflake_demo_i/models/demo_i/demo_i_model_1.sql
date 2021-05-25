c172d091e1a3c47b589fae72a2507cfec517821f
Peiran Quan <quanpeiran@gmail.com>
2021-05-25 13:20:36 +1200

    disable example models

--- dbt_snowflake_demo_i/models/demo_i/demo_i_model_1.sql
+++ dbt_snowflake_demo_i/models/demo_i/demo_i_model_1.sql
@@ -1,9 +1,9 @@
 with source_fact as (
     select * from {{source('demo_i_sample_data', 'fact')}}
-)
-with source_dim_city as (
+),
+source_dim_city as (
     select * from {{source('demo_i_sample_data', 'dim_city')}}
-)
+),
 final as (
     select f.Team, Opponet, Date, Win_Loss, dc.Team, Location, Rate
     from source_fact f 
